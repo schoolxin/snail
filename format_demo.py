54bb89c924d8f0518562ec45dcba31aa75d01d33
dengzz <707643733@qq.com>
2023-05-30 10:29:17 +0800

    .

--- format_demo.py
+++ format_demo.py
@@ -7,3 +7,18 @@ print("%(name)sAnd%(age)d" % {"name": "hello", "age": 90})
 
 age = 25
 print(f"我今年{age}岁")
+
+import math
+
+print(math.ceil(10.2))
+print(math.ceil(-10.2))
+
+print(math.floor(10.2))
+print(math.floor(-10.2))
+print(math.fsum([x for x in range(101)]))
+a = math.sqrt(10)
+
+print("{a:.2f}".format(a=a))
+print(f"{a:.2f}")
+print(f"{a:.4f}")
+print("%.4f" % a)
