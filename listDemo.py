cdf644e3bd53e7cf91fb31a0350588008c5e2758
dengzz <707643733@qq.com>
2023-06-12 11:21:58 +0800

    .

--- listDemo.py
+++ listDemo.py
@@ -3,7 +3,7 @@
 # @Time      :2023/6/9 9:34
 # @Author    :dzz
 # @Function  :
-list1 = [1,2,3,4,5]
+list1 = [1,2,3,4,5,4]
 for i in list1:
     print(i)
 # print(max(list1))
@@ -14,4 +14,15 @@ list1.append([100,299,99])
 list1.extend([[100,299,99]])
 print(list1)
 # print(list1.count(1))
-print(list1.index(1000))
+# print(list1.index(1000))
+
+# print(list1.pop())
+# print(list1.remove(100))
+print(list1)
+
+al=[1,2,3,4,5,4,'34']
+print(al)
+al.sort(key=lambda x:int(x))
+print(al)
+print(al.reverse())
+print(al)
