1e5d37974f27b80e2bc6d603ffdb409c22b8aa5e
dengzz <707643733@qq.com>
2023-06-18 23:08:54 +0800

    .

--- dictDemo.py
+++ dictDemo.py
@@ -16,5 +16,39 @@ print(dic1)
 
 # del dic1['name']
 print(dic1)
-print(dic1.pop('name'))
-print(dic1.popitem())
+# print(dic1.pop('name'))
+# print(dic1.popitem())
+print(len(dic1))
+
+# 浅拷贝
+dic2 = dic1
+print(dic2)
+dic2['name'] = "woniuniu"
+print(dic1)
+# 深拷贝
+dic3 = dic1.copy()
+dic3.update({"name": "蜗牛"})
+
+print(dic3)
+print(dic1)
+
+alist = ['name', 'age', 'address']
+alist2 = [1, 2, 3]
+d4 = dict.fromkeys(alist, alist2)
+d5 = dict(zip(alist, alist2))
+print("d5", d5)
+for item in d5.items():
+    print(item)
+
+print("setdefault")
+print(d5.setdefault("name1", "jack"))
+print(d5)
+d5.update({"name": "eee", 'height': 100})
+print(d5)
+print(d5.pop('name'))
+print(d5.pop('name5', 'not exists'))
+print(d5.popitem())
+print(d5)
+print("循环删除字典中的元素")
+while len(d5.keys())>0:
+    print(d5.popitem())
