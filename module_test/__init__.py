f6c5747edb6f977556dc03afc6f58e284c173941
dengzz <707643733@qq.com>
2023-06-25 09:37:47 +0800

    .

---
+++ module_test/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# @FileName  :__init__.py.py
+# @Time      :2023/6/25 9:11
+# @Author    :dzz
+# @Function  :
+
+
+if __name__ == "__main__":
+    run_code = 0
---
+++ module_test/py1.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# @FileName  :py1.py
+# @Time      :2023/6/25 9:11
+# @Author    :dzz
+# @Function  :
+
+def mysum(a,b):
+    return  a + b
+if __name__ == '__main__':
+    mysums = mysum(4,5)
+    print("mysum",mysums)
+    print("模块名",__name__)
