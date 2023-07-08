f2f701faa7ba0c377327674a45bb08cfeaa6a245
dengzz <707643733@qq.com>
2023-07-08 11:56:08 +0800

    .

--- woniuatm/test.py
+++ woniuatm/test.py
@@ -3,19 +3,12 @@
 # @Time      :2023/7/7 9:10
 # @Author    :dzz
 # @Function  :
-usename=["2"]
+
+flags = True
 def login():
-    uname = input("请输入用户名")
-    print("外",id(uname))
-    while True:
-
-        for i in range(10):
-            print(i)
-            if i ==8:
-                break
-
+    global flags
+    flags = False
 login()
 
-l1 = [['a','2'],['b','3'],'a']
-if 'a' in l1:
-    print("exists")
+
+print(flags)
