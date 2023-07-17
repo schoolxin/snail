1541372bba0c80d6ecf351b7416aa44d9d3527ae
dengzz <707643733@qq.com>
2023-07-17 23:15:22 +0800

    .

--- python_advance/decorator_demo3.py
+++ python_advance/decorator_demo3.py
@@ -5,9 +5,10 @@ def timer(timer_type):
     def outer(func): # 加入一层嵌套函数，并且接收被装饰函数名作为参数
         def inner(*args, **kwargs):
             start_time = time.time()
-            func(*args, **kwargs)
+            res = func(*args, **kwargs)
             end_time = time.time()
             print("运行fun的时间为{}".format(end_time - start_time))
+            return res
 
         return inner # 返回inner的引用
 
@@ -18,10 +19,11 @@ def timer(timer_type):
 def foo(name, age):
     time.sleep(3)
     print("in foo", name, age)
+    return name
 
 
 # foos = timer(foo)
 # foos()
 
 # foo('woniu')  # 相当于将foo函数传递给timer函数(装饰器)
-foo('woniu', 98)  # 相当于将foo函数传递给timer函数(装饰器)
+print(foo('woniu', 98))  # inner(*args, **kwargs)
