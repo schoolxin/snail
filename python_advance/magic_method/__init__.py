ed2584763a9701e45e5d69da27e01fb2b06f65d0
dengzz <707643733@qq.com>
2023-07-24 10:49:56 +0800

    .

---
+++ python_advance/magic_method/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# @FileName  :__init__.py.py
+# @Time      :2023/7/24 9:47
+# @Author    :dzz
+# @Function  :
+
+
+if __name__ == "__main__":
+    run_code = 0
---
+++ python_advance/magic_method/sp_demo01.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+# @FileName  :sp_demo01.py
+# @Time      :2023/7/24 9:47
+# @Author    :dzz
+# @Function  :
+class ob1:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def __repr__(self):
+        return "姓名{nm}和年龄{ag}".format(nm = self.name,ag = self.age)
+
+if __name__ == "__main__":
+    o1 = ob1('zhan',1000)
+    print(o1)
+    # print(o1.__repr__())
