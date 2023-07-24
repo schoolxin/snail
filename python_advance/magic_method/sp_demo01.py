61dc1e6dd132eb6c5d36eb10ce520983c1c1c2b4
dengzz <707643733@qq.com>
2023-07-24 23:18:44 +0800

    .

--- python_advance/magic_method/sp_demo01.py
+++ python_advance/magic_method/sp_demo01.py
@@ -4,13 +4,15 @@
 # @Author    :dzz
 # @Function  :
 class ob1:
-    def __init__(self,name,age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
+
     def __repr__(self):
-        return "姓名{nm}和年龄{ag}".format(nm = self.name,ag = self.age)
+        return "姓名{nm}和年龄{ag}".format(nm=self.name, ag=self.age)
+
 
 if __name__ == "__main__":
-    o1 = ob1('zhan',1000)
+    o1 = ob1('zhan', 1000)
     print(o1)
     # print(o1.__repr__())
