c374ac6febd30934dd6c506a9ae0321156806c8b
dengzz <707643733@qq.com>
2023-07-31 23:12:39 +0800

    .

--- socket_chat/group_chat/client.py
+++ socket_chat/group_chat/client.py
@@ -4,19 +4,26 @@
 # @Author    :dzz
 # @Function  :
 
-import socket
+import socket, threading
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPV4 TCP
 
 client.connect(('127.0.0.1', 8000))
 
-# client.send('woniu'.encode('utf8'))
-while True:
-    to_data = input("客户端输入:")
-    client.send(to_data.encode('utf8'))  # 想服务器发送消息
-    data = client.recv(1024)
-    print(data.decode('utf8'))
-    if data.decode('utf8') == '886':
-        break
 
-client.close()
+# 接收消息
+def receive(client):
+    while True:
+        data = client.recv(1024)
+        print(data.decode('utf8'))
+
+
+# 发送消息
+def send(client):
+    while True:
+        to_data = input("客户端输入:")
+        client.send(to_data.encode('utf8'))  # 想服务器发送消息
+
+
+threading.Thread(target=receive, args=(client,)).start()
+threading.Thread(target=send, args=(client,)).start()
--- socket_chat/group_chat/server.py
+++ socket_chat/group_chat/server.py
@@ -19,21 +19,28 @@ client_sock = []
 
 
 def handle_chat(sock):
-    while True:
-        data = sock.recv(1024)  # 获取数据的大小 一次接收1K
-        # 服务器接收到数据后进行转发给其他客户端
-        # send_data = data.encode()
-        if client_sock:
-            for client in client_sock:
-                client.send(data)
-        print("消息转发成功！！！")
-        # print(data.decode('utf8'))  # 通过socket接收到数据是bytes类型，只有在解码之后才能正确显示
-        # re_data = input("输入:")
-        # sock.send(re_data.encode())
+    try:
+        user_name = sock.recv(1024).decode()
+        for client in client_sock:
+            client.send(("欢迎" + user_name + "加入群聊").encode())
+        while True:
+            data = sock.recv(1024).decode()  # 获取数据的大小 一次接收1K
+            # print("接收到消息{}".format(data.decode()))
+            # 服务器接收到数据后进行转发给其他客户端
+            # send_data = data.encode()
+            if client_sock:
+                for client in client_sock:
+                    client.send((user_name + "说:" + data).encode())
+            print("消息转发成功！！！")
+    except Exception as e:
+        client_sock.remove(sock)
+        for client in client_sock:
+            client.send("{}已经退出群聊".format(user_name).encode())
 
 
 while True:
     sock, addr = server.accept()  # 阻塞方法 等待客户端的链接 sock 当前的链接对象     addr 表示链接上来的地址 可以区分不同的客户端  接受不同的客户端链接请求
+    sock.send("请输入您的昵称".encode())
     client_sock.append(sock)
     # 每连接上来一个客户端就创建一个线程与之交互
     thread_client = threading.Thread(target=handle_chat, args=(sock,))
