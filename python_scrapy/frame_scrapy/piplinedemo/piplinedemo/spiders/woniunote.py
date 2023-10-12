4a86e47762ecc182a82c18d0844a3edcb42e88ca
dengzz <707643733@qq.com>
2023-10-12 23:32:40 +0800

    .

---
+++ python_scrapy/frame_scrapy/piplinedemo/piplinedemo/spiders/woniunote.py
@@ -0,0 +1,46 @@
+import re
+
+import scrapy
+
+from ..items import PiplinedemoItem
+
+
+class WoniunoteSpider(scrapy.Spider):
+    name = "woniunote"
+    allowed_domains = ["www.woniunote.com"]
+
+    # start_urls = ["http://www.woniunote.com"]
+
+    def start_requests(self):
+        base_url = "https://www.woniuxy.com/note/page-{}"
+
+        for page in range(1, 11):
+            url = base_url.format(str(page))
+            # 直接把带请求的地址扔到scrapy的请求队列中  dont_filter=True 不要让scrapy 自动过滤掉相似的请求
+            yield scrapy.Request(url=url, callback=self.parse_list, dont_filter=True)
+
+    def parse(self, response):
+        pass
+
+    def parse_list(self, response):
+        # content_url = response.xpath("//div[@class='title']/a/@href").extract()
+        content_url = response.xpath("//div[@id='content']/a/@href").getall()
+        # print("content_url", content_url)
+        for url in content_url:
+            # print(response.urljoin(url))
+            yield scrapy.Request(url=response.urljoin(url),
+                                 callback=self.parse_content,
+                                 dont_filter=True
+                                 )
+
+    def parse_content(self, response):
+
+        items = PiplinedemoItem()
+        items['title'] = response.xpath("//h1[@class='content-title']/text()").extract()[0].strip()
+
+
+        items['date'] = response.xpath("//p[@class='date-year']/text()").extract()[0].strip() + "/" + \
+                        response.xpath("//p[@class='date-time']/text()").extract()[0].strip()
+        print(items)
+        # items['read'] = re.findall("阅读：(.*?)\s+", response.text, re.S)[0]
+        # return items
--- python_scrapy/usecookielogin.py
+++ python_scrapy/usecookielogin.py
@@ -15,38 +15,38 @@ from faker import Faker
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-    "Content-Type": "application/json"
+    "X-Requested-With":"XMLHttpRequest"
 }
-#
+#l
 #
 # # sess = requests.Session()
 # resp = requests.get("https://woniuxy.com/",headers=headers)
 #
 # print(resp.text)
 # 多步操作要使用session
-# sess = requests.Session()
-# resp = sess.get("https://woniuxy.com/sys/user/captcha?t=Math.random()")
-# with open('checkcode.png', 'wb') as file:
-#     file.write(resp.content)
-#
-# checkcode = input("请输入验证码")
-# datas = {
-#     "tel": "15591819289",
-#     "password": "708170",
-#     "captcha": checkcode,
-#     "loginType": 1
-# }
-#
-# resp2 = sess.post("https://woniuxy.com/sys/user/login", data=json.dumps(datas),headers=headers)
-# print(resp2.text)
+sess = requests.Session()
+resp = sess.get("https://woniuxy.com/sys/user/captcha?t=Math.random()")
+with open('checkcode.png', 'wb') as file:
+    file.write(resp.content)
+
+checkcode = input("请输入验证码")
+datas = {
+    "tel": "15591819289",
+    "password": "708170",
+    "captcha": checkcode,
+    "loginType": 1
+}
+
+resp2 = sess.post("https://woniuxy.com/sys/user/login", data=json.dumps(datas),headers=headers)
+print(resp2.text)
 # sess.post("https://woniuxy.com/user/authstudent/433")
 
 
-print("github")
-resp = requests.get("https://github.com/login", headers=headers)
-
-soup = BeautifulSoup(resp.text, 'lxml')
-
-authenticity_token = soup.select_one('input[name="authenticity_token"]').get('value')
-
-print(authenticity_token)
+# print("github")
+# resp = requests.get("https://github.com/login", headers=headers)
+#
+# soup = BeautifulSoup(resp.text, 'lxml')
+#
+# authenticity_token = soup.select_one('input[name="authenticity_token"]').get('value')
+#
+# print(authenticity_token)
